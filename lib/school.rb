656c562a324ec6d8034d424a62543704dbf95f49
Blythe Pollard <blytheepollard@gmail.com>
2019-09-28 22:19:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,8 +20,8 @@ class School
   end
   
   def sort
-     roster.values.sort_by { |key, value| value}
-     #value.sort_by {|first, last| first}.to_h
+     roster.sort_by { |key, value| value}
+     value.values.sort_by {|first, last| first}.to_h
     
   end
 end
